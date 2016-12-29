7ba6de3d2f22f2a845b577261309c9547a51010a
Adrien Sartori <adrien.sartori@alumni.univ-avignon.fr>
2016-12-29 16:07:04 +0100

    correction d'une variable

--- Tamagotchi.cpp
+++ Tamagotchi.cpp
@@ -170,7 +170,7 @@ Tamagotchi::Tamagotchi() {
 		//int joue = atoi(get_humeur());
 		int fatiguee = get_fatigue();
 		//set_humeur(joue+10);
-		set_fatigue(fatigue+10);
+		set_fatigue(fatiguee+10);
 	}
 	
 	/*void Tamagotchi::dormir() // fonction qui régénère la fatigue suivant le temps entre deux sessions.
